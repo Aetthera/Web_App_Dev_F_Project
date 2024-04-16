2a84eba3545b72fc03cfd96c94608893d353fcfe
Alena <alenabelovadmvn@gmail.com>
2024-04-16 00:38:13 -0400

    Bootstrap styling added

--- dashboard.php
+++ dashboard.php
@@ -7,11 +7,25 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
     <head>
         <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     </head>
 
     <body>
-        <h1>Hello, <?php echo $_SESSION['user_name']; ?></h1>
-        <a href="logout.php">Logout</a>
+        <div class="col-lg-4 offset-lg-4 bg-body-secondary rounded">
+            <h1 class="text-center mt-2">Hello, <?php echo $_SESSION['user_name']; ?></h1>
+            <div class="d-grid col-md-5 mx-auto">
+                <button type="button" class="btn btn-primary mt-3 "><a href="logout.php">
+                        <p class="text-light m-0" style="font-size: 24px">Logout</p>
+                    </a></button>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
     </body>
 
     </html>
