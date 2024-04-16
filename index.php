caa965f7097c2b8ba46a231417d5fe1bd927108b
Alena <alenabelovadmvn@gmail.com>
2024-04-16 00:38:31 -0400

    title changed

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="author" content="Sahil Kumar">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>User Registration and Login</title>
+    <title>Login | Registration</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link type="text/css" rel="stylesheet" href="/global.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
