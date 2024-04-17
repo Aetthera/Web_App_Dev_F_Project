57bfe97f140383911081d2313a70715a0c7eb751
Alena <alenabelovadmvn@gmail.com>
2024-04-17 14:57:54 -0400

    register.php added but not completed

--- login.php
+++ login.php
@@ -30,7 +30,6 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) === 1) {
     $row = mysqli_fetch_assoc($result);
     if ($row['user_name'] === $uname && $row['password'] === $pass) {
-        echo "Logged In!";
         $_SESSION['user_name'] = $row['user_name'];
         $_SESSION['name'] = $row['name'];
         $_SESSION['id'] = $row['id'];
---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+include "db_conn.php";
+
+if (isset($_POST['uname']) && isset($_POST['pass']) && isset($_POST['cpass']) && isset($_POST['fanme']) && isset($_POST['lname'])) {
+
+
+    function validate($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+$uname = validate($_POST['uname']);
+$pass = validate($_POST['pass']);
+$cpass = validate($_POST['cpass']);
+$fname = validate($_POST['fname']);
+$lname = validate($_POST['lname']);
+
+if (empty($uname)) {
+    header("Location: index.php?error=Username field cannot be empty");
+    exit();
+} else if (empty($pass)) {
+    header("Location: index.php?error=Password field cannot be empty");
+    exit();
+} else if (empty($cpass)) {
+    header("Location: index.php?error=Confirmation Password field cannot be empty");
+    exit();
+} else if (empty($fname)) {
+    header("Location: index.php?error=First Name field cannot be empty");
+    exit();
+} else if (empty($lname)) {
+    header("Location: index.php?error=Last Name field cannot be empty");
+    exit();
+}
